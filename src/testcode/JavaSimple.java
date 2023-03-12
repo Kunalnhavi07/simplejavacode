6d58909e3596bf588d141df9236d3dfa5fcece8b
HP <HP@DESKTOP-5T8Q470>
2023-03-12 14:11:04 +0530

    Signed-off-by: HP <HP@DESKTOP-5T8Q470>

---
+++ src/testcode/JavaSimple.java
@@ -0,0 +1,16 @@
+package testcode;
+
+public class JavaSimple {
+
+	public static void main(String[] args) {
+		int i=1;
+		int n=50;
+		
+		for(i=1; i<=n; i++)
+		System.out.print(i+" ");
+
+
+
+	}
+
+}
